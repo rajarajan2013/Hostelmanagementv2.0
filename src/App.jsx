01185038969fb5c840c7c9dbf773483e7616831d
ritveek-rana <ritveekrana@gmail.com>
2023-01-23 23:35:57 +0530

    template cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import forgot from './pages/forgot'
 import login from './pages/login'
 import signup from './pages/signup'
+import superadmin from './pages/superadmin'
+import hostel from './pages/hostel'
+import header from './components/header'
+import nav from './components/navbar'
 import { Route,Routes } from 'react-router-dom'
 
 function App () {
---
+++ src/pages/superadmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function superadmin() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default superadmin
