4eb320e7191ee257e49d540478c92c716da7a33f
rajarajan2013 <rajarajanmalathi13@gmail.com>
2023-01-26 19:28:58 +0530

    authentication routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import Forgot from './pages/forgot'
 import Login from './pages/login'
 import Signup from './pages/signup'
-import Superadmin from './pages/superadmin'
-import Hostel from './pages/hostel'
-import Header from './components/header'
-import Nav from './components/navbar'
+// import Superadmin from './pages/superadmin'
+// import Hostel from './pages/hostel'
+// import Header from './components/header'
+// import Nav from './components/navbar'
 import { Route,Routes } from 'react-router-dom'
 
 function App () {
       return(
-        <div>
-          <Login/>
-          <Signup/>
-          <Forgot/>
-        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signup" element={<Signup/>} />
+            <Route path="/forgotpassword" element={<Forgot/>} />
+          </Routes>
+        </BrowserRouter>  
   )
 }
 
