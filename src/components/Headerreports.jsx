1aa40d3f889e9dd6d14c3b2330eaad694a8fb86b
rajarajan2013 <rajarajanmalathi13@gmail.com>
2023-02-01 14:34:08 +0530

    ..

--- src/components/Headerreports.jsx
+++ src/components/Headerreports.jsx
@@ -7,7 +7,8 @@ function Headerreports() {
       <button type="button2" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Modify</button>
       <button type="button3" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Save</button>
       <button type="button4" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Done</button>
-    </div><div class="container flex mx-auto">
+    </div>
+     <div class="container flex mx-auto">
         <div class="flex border-2 rounded">
           <button class="flex items-center justify-center px-4 border-r bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700">
             <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
