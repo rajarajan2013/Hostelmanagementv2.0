3a19749d117afdf9dda13556227c747ed478850e
rajarajan2013 <rajarajanmalathi13@gmail.com>
2023-02-01 14:24:33 +0530

    fixes

--- src/pages/dashboard/bookingrep.jsx
+++ src/pages/dashboard/bookingrep.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Headerreports from '../../components/Headerreports'
 import Nav from "../../components/navbar"
 
 const para =["S.No","Student Name","Registration Number","Branch","Room No.","Booking Type","Total Amount"]
@@ -9,25 +10,7 @@ function Bookingrep() {
         <>
         <div>
             <Nav/> 
-            <div class="button01">
-            <button type="button1" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">+Add</button>
-            <button type="button2" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Modify</button>
-            <button type="button3" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Save</button>
-            <button type="button4" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Done</button>
-            </div>
-            <div class="container flex mx-auto">
-            <div class="flex border-2 rounded">
-            <button class="flex items-center justify-center px-4 border-r bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700">
-            <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24">
-                <path
-                    d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z">
-                </path>
-            </svg>
-            </button>
-            <input type="text" class="px-4 py-2 w-80" placeholder="Search..."></input>
-            </div>
-        </div>
+            <Headerreports/>
                 <div class="relative overflow-x-auto">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 border-solid border-2 border-black">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-solid border-2 border-black">
