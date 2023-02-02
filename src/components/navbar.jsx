81882dc8376b93e465e0f6e5bf908e1a374e979c
rajarajan2013 <rajarajanmalathi13@gmail.com>
2023-02-02 09:46:11 +0530

    romving rooms from navbar for phase 2 of the proje

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -27,9 +27,9 @@ function navbar() {
               <li>
                 <a href="/customrep" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Custom Report</a>
               </li>
-              <li>
+              {/*<li>
                 <a href="/rooms" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Rooms</a>
-              </li>
+  </li>*/}
             </ul>
           </div>
         </div>
