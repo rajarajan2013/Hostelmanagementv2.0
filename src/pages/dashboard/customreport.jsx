d6a7b12e000a4cbeb7a40117380ceb2ebb271380
Ramanagiri <ra3451@srmist.edu.in>
2023-02-02 21:37:08 +0530

    customrep

--- src/pages/dashboard/customreport.jsx
+++ src/pages/dashboard/customreport.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import Nav from "../../components/navbar"
-import Scroll from "../../components/scroll"
+import './Masterrep.css' 
 
-// const tablehead = ["S.No","Student ID","Student Name","Register No.", "Gender","Degree","Semester","Current Year","Branch","Booking Date","Hostel Name","Room Number","Room Type","Floor Name","Contact No.","Email","Father Name","Father Number","Father Email","Address","Pincode","Office Name","Booking Type"]
+// const tablehead = ["S.No","Student ID","Student Name","Register No.", "Gender","Degree","Semester","Current Year","Branch","Booking Date","HostelName Name","Room Number","Room Type","Floor Name","Contact No.","Email","Father Name","Father Number","Father Email","Address","Pincode","Office Name","Booking Type"]
 
 
 function Customrep() {
     return (
         <>
          <Nav/>
-         <Scroll/>
-         
+         <div className='scrollbar'>
                 <table className="table">
                     <tbody><tr>
                         <th>S.No #</th>
@@ -20,6 +19,7 @@ function Customrep() {
                         <td><label for="cr1">1.</label></td>
                         <td>
                             <select className="opt form-control" fdprocessedid="q9ba6rk">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -39,6 +39,7 @@ function Customrep() {
                         
                         <td>
                             <select className="opt form-control" fdprocessedid="d3p31">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -58,6 +59,7 @@ function Customrep() {
                         
                         <td>
                             <select className="opt form-control" fdprocessedid="f0ij9">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -77,6 +79,7 @@ function Customrep() {
                         
                         <td>
                             <select className="opt form-control" fdprocessedid="e9n4m">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -96,6 +99,7 @@ function Customrep() {
                         
                         <td>
                             <select className="opt form-control" fdprocessedid="af1hyp">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -115,6 +119,7 @@ function Customrep() {
                        
                         <td>
                             <select className="opt form-control" fdprocessedid="efwxg5">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -134,6 +139,7 @@ function Customrep() {
                        
                         <td>
                             <select className="opt form-control" fdprocessedid="i9irka">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -153,6 +159,7 @@ function Customrep() {
                         
                         <td>
                             <select className="opt form-control" fdprocessedid="473l7">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -172,6 +179,7 @@ function Customrep() {
                     
                         <td>
                             <select className="opt form-control" fdprocessedid="g34bj">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -191,6 +199,7 @@ function Customrep() {
                        
                         <td>
                             <select className="opt form-control" fdprocessedid="0mth7n">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -210,6 +219,7 @@ function Customrep() {
                       
                         <td>
                             <select className="opt form-control" fdprocessedid="92uw8w">
+                                <option>None</option>
                                 <option>HostelName</option>
                                 <option>FloorNo</option>
                                 <option>RoonNo</option>
@@ -223,9 +233,14 @@ function Customrep() {
                             </select>
                         </td>
                     </tr>
-                <tr><td>12.</td><td><select className="opt form-control" fdprocessedid="inivd8"><option>HostelName</option><option>FloorNo</option><option>RoonNo</option><option>PhoneNo</option><option>FatherNo</option><option>Address</option><option>CurrentYear</option><option>Degree</option><option>I</option></select></td></tr><tr><td>13.</td><td><select className="opt form-control" fdprocessedid="5airz"><option>HostelName</option><option>FloorNo</option><option>RoonNo</option><option>PhoneNo</option><option>FatherNo</option><option>Address</option><option>CurrentYear</option><option>Degree</option><option>I</option></select></td></tr></tbody></table>
-        
-        
+                <tr><td>12.</td><td><select className="opt form-control" fdprocessedid="inivd8"><option>None</option>
+                <option>HostelName</option><option>FloorNo</option><option>RoonNo</option><option>PhoneNo</option><option>FatherNo</option><option>Address</option><option>CurrentYear</option><option>Degree</option><option>I</option></select></td></tr><tr><td>13.</td><td><select className="opt form-control" fdprocessedid="5airz"><option>None</option>
+                <option>HostelName</option><option>FloorNo</option><option>RoonNo</option><option>PhoneNo</option><option>FatherNo</option><option>Address</option><option>CurrentYear</option><option>Degree</option><option>I</option></select></td></tr></tbody></table>
+                
+                <button type="button1" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-4 mb-2">+Add Field</button>
+                <button type="button1" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-4 mb-2">Submit</button>
+                
+                </div>
       </>  
         
     )
