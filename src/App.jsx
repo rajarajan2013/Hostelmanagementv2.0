2bdd7b837857c17e713a4397134b1dee0cffd267
rajarajan2013 <rajarajanmalathi13@gmail.com>
2023-02-02 09:39:10 +0530

    Routing and naming convention fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Forgot from './pages/authentication/forgot'
 import Login from './pages/authentication/login'
 import Signup from './pages/authentication/signup'
 import Bookingrep from "./pages/dashboard/bookingrep"
-import Report from "./pages/dashboard/masterrep"
+import Masterrep from "./pages/dashboard/masterrep"
 import Home from "./pages/dashboard/home"
-import Attendance from './pages/dashboard/attendancerep'
+import Attendancerep from './pages/dashboard/attendancerep'
 import Masterrep from './pages/dashboard/masterrep'
+import Customrep from './pages/dashboard/customreport'
 
 function App () {
       return(
@@ -20,9 +21,9 @@ function App () {
             <Route path="/forgotpassword" element={<Forgot/>} />
             <Route path="/bookingrep" element={<Bookingrep/>} />
             <Route path="/masterrep" element={<Masterrep/>} />
-            <Route path="/report" element={<Report/>} />
+            <Route path="/customreport" element={<Customrep/>} />
             <Route path="/home" element={<Home/>} />
-            <Route path="/attendance" element={<Attendance/>} />
+            <Route path="/attendancerep" element={<Attendancerep/>} />
           </Routes>
         </BrowserRouter>  
   )
